1f640f95a41ef222010ebbaa2caf7240b02d4fdb
f-zamanan <f.n.zamanan@gmail.com>
2025-04-27 18:06:01 +0300

    arrays exercise during class

---
+++ arrays.ts
@@ -0,0 +1,18 @@
+// arrays small excersie
+// arrays are not static there are push and pop methods to dynamically change the length of the array by adding/removing values
+// there are different methods to dynamically manipulate an arrays
+
+let favMovies: string[] = [
+  "Avengers",
+  "Avengers: age of ultron",
+  "Avengers: infinity war",
+  "Avengers: endgame",
+  "iron man",
+];
+let kuwaitWeeklyTemp: number[] = [34, 32, 33, 36, 36, 38, 36];
+
+console.log(favMovies.length);
+
+favMovies.push("Iron man 2");
+
+console.log(favMovies.length);
